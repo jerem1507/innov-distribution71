b02d2cd76ba4fd68d8bd081b6020c60f6a3eff95
Jeremy PANETTO <jeremy.panetto@clearstream.com>
2023-03-17 12:23:01 +0100

    front page

--- innov-distribution71/src/app/app.module.ts
+++ innov-distribution71/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
